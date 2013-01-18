253c29735a0e6800de0203f1d5a913e6e97ac53b
Alex Tereshkin <atereshkin@y-node.com>
2013-01-18 13:51:23 +0300

    Mistype

--- mongo.py
+++ mongo.py
@@ -30,7 +30,7 @@ class MongoBackend(object):
                        
     def label_visitor(self, 
                       visitor_id,
-                      lablel):
+                      label):
         visitor = visitors.find_one({'id' : visitor_id})
         labels = visitor.get('labels', [])
         labels.append(label)
